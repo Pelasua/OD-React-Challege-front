716d21f8d8fd9d182675282e41749d2a10647372
Pelayo Suárez <pesuafer@gmail.com>
2023-01-15 09:29:14 +1000

    feat: added general styles with the same method

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+/** @jsxImportSource @emotion/react */
 import React, { createContext, useState } from 'react'
+import { css } from '@emotion/react'
+import Box from '@mui/material/Box'
+
+import '@fontsource/roboto'
 
 import TopMenu from './infrastructure/ui/components/TopMenu'
 import Gallery from './infrastructure/ui/pages/Gallery'
 import PageDetails from './infrastructure/ui/pages/PageDetails'
+
+import * as appStyles from './App.scss'
 
 export const AppContext = createContext(null)
 function App () {
@@ -15,12 +22,12 @@ function App () {
   })
 
   return (
-    <div className="App">
+    <Box className="App" css={css`${appStyles}`}>
       <AppContext.Provider value={{ store, actions }}>
         <TopMenu title={store.selectedPage === 'gallery' ? 'Gallery'.toUpperCase() : 'Page Details'.toUpperCase()}/>
         {store.selectedPage === 'gallery' ? <Gallery/> : <PageDetails/>}
       </AppContext.Provider>
-    </div>
+    </Box>
   )
 }
 
--- src/infrastructure/ui/pages/PageDetails/index.js
+++ src/infrastructure/ui/pages/PageDetails/index.js
@@ -1,4 +1,3 @@
-/** @jsxImportSource @emotion/react */
 import React from 'react'
 import { css } from '@emotion/react'
 import Box from '@mui/material/Box'
