ab6b859647b1f1ac7a6ca2d09b20716f8ea9febd
Pelayo Suárez <pesuafer@gmail.com>
2023-01-14 17:52:16 +1000

    feat: added a global store

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, { } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
 import TopMenu from './infrastructure/ui/components/TopMenu'
 
 import './App.css'
 
+export const AppContext = createContext(null)
 function App () {
+  const [store, setStore] = useState({
+    selectedPage: 'gallery'
+  })
+  // eslint-disable-next-line no-unused-vars
+  const [actions, setActions] = useState({
+    selectPage: (page) => setStore({ ...store, selectedPage: page })
+  })
+
+  useEffect(() => {
+    console.log('Ha cambiado')
+  }, [store])
+
   return (
     <div className="App">
-      <TopMenu/>
-
+      <AppContext.Provider value={{ store, actions }}>
+        <TopMenu title={store.selectedPage === 'gallery' ? 'Gallery' : 'Page Details'}/>
+      </AppContext.Provider>
     </div>
   )
 }
--- src/infrastructure/ui/components/TopMenu/index.js
+++ src/infrastructure/ui/components/TopMenu/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import MenuIcon from '@mui/icons-material/Menu'
 import MoreIcon from '@mui/icons-material/MoreVert'
 import AppBar from '@mui/material/AppBar'
@@ -9,13 +9,22 @@ import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import PropTypes from 'prop-types'
 
+import { AppContext } from '../../../../App'
+
 function TopMenu ({ title, isTransparent }) {
+  const context = useContext(AppContext)
   const [anchorEl, setAnchorEl] = useState(null)
   const open = Boolean(anchorEl)
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget)
   }
   const handleClose = () => {
+    setAnchorEl(null)
+  }
+  const handlePage = (event) => {
+    const page = event.target.getAttribute('data-page-name')
+
+    context.actions.selectPage(page)
     setAnchorEl(null)
   }
 
@@ -41,8 +50,8 @@ function TopMenu ({ title, isTransparent }) {
           'aria-labelledby': 'basic-button'
         }}
       >
-        <MenuItem onClick={handleClose}>Gallery</MenuItem>
-        <MenuItem onClick={handleClose}>Detail page</MenuItem>q
+        <MenuItem data-page-name="gallery" onClick={(handlePage)}>Gallery</MenuItem>
+        <MenuItem data-page-name="detailPage" onClick={handlePage}>Detail page</MenuItem>
       </Menu>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             {title}
