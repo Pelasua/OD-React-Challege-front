6c51da38c48b46fcf84aa57fc09e13b043ee153e
Pelayo Suárez <pesuafer@gmail.com>
2023-01-14 16:03:35 +1000

    chore: added config files

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard'],
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              ['^react', '^@?\\w'],
+
+              ['^(@|components)(/.*|$)'],
+
+              ['^\\u0000'],
+
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+
+              ['^.+\\.?(css)$']
+            ]
+          }
+        ],
+        'newline-after-var': ['error', 'always'],
+        'import/newline-after-import': ['error', { count: 1 }]
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', 'simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error'
+  }
+}
