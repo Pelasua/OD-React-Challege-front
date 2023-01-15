197942650f8006b2454694fe2af77ce0423c2f1b
Pelayo Suárez <pesuafer@gmail.com>
2023-01-15 17:27:33 +1000

    feat: added box shadow to gallery menu

--- src/infrastructure/ui/pages/Gallery/index.js
+++ src/infrastructure/ui/pages/Gallery/index.js
@@ -40,8 +40,8 @@ function Gallery () {
         <>
           <Box
             css={css`
-              border-bottom: 1px solid;
               border-color: rgba(0, 0, 0, 0.12);
+              box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)
             `}
           >
             <Tabs
