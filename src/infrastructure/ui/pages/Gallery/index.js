34a60b4188f632cb3fd256b512073d4f07b3a8bf
Pelayo Suárez <pesuafer@gmail.com>
2023-01-15 11:11:39 +1000

    feat: added styles to the tab component

--- src/infrastructure/ui/pages/Gallery/index.js
+++ src/infrastructure/ui/pages/Gallery/index.js
@@ -12,10 +12,34 @@ function Gallery () {
   }
 
   return (
-    <Box css={css`
-    padding-top: 70px`}>
-     <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} variant="fullWidth" >
+    <Box
+      css={css`
+        padding-top: 70px;
+      `}
+    >
+      <Box
+        css={css`
+          border-bottom: 1px solid;
+          border-color: rgba(0, 0, 0, 0.12);
+        `}
+      >
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="fullWidth"
+          css={css`
+            & .css-1aquho2-MuiTabs-indicator {
+              height: 3px;
+              background-color: rgb(164, 125, 214);
+            }
+            & .css-1ujykiq-MuiButtonBase-root-MuiTab-root {
+              font-weight: bold;
+              &.Mui-selected {
+                color: gray;
+              }
+            }
+          `}
+        >
           <Tab label="Videos" />
           <Tab label="Photos" />
         </Tabs>
